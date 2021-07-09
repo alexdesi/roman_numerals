d271302f5e13f0c8d65a059128f043a996720565
Jonathan James <jonathan.james@fundingcircle.com>
2021-07-09 17:34:16 +0100

    Make single_digit private.

--- lib/roman_converter.rb
+++ lib/roman_converter.rb
@@ -1,32 +1,36 @@
 class RomanConverter
-  def self.to_roman(decimal)
-    tens, ones = decimal.divmod(10)
-    roman = ''
+  class << self
+    def to_roman(decimal)
+      tens, ones = decimal.divmod(10)
+      roman = ''
 
-    if tens >= 5
-      roman += 'L'
-      tens -= 5
+      if tens >= 5
+        roman += 'L'
+        tens -= 5
+      end
+
+      if tens == 4
+        roman += 'XL'
+        tens -= 4
+      end
+
+      roman += 'X' * tens
+      roman + single_digit(ones)
     end
 
-    if tens == 4
-      roman += 'XL'
-      tens -= 4
+    private
+
+    def single_digit(decimal)
+      return 'IX' if decimal == 9
+      return 'IV' if decimal == 4
+
+      roman = ''
+      if decimal >= 5
+        roman += 'V'
+        decimal -= 5
+      end
+
+      roman + ('I' * decimal)
     end
-
-    roman += 'X' * tens
-    roman + single_digit(ones)
-  end
-
-  def self.single_digit(decimal)
-    return 'IX' if decimal == 9
-    return 'IV' if decimal == 4
-
-    roman = ''
-    if decimal >= 5
-      roman += 'V'
-      decimal -= 5
-    end
-
-    roman + ('I' * decimal)
   end
 end
