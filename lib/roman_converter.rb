b695cb597dbdaec23433750a6fddb09b7da6b890
Frank Kair <frank.kair@fundingcircle.com>
2021-07-09 12:03:57 +0100

    Refactors testing code & adds single_digit method

--- lib/roman_converter.rb
+++ lib/roman_converter.rb
@@ -1,16 +1,32 @@
 class RomanConverter
   def self.to_roman(decimal)
     tens, ones = decimal.divmod(10)
-    roman = 'X' * tens
-    if ones >= 5
-      roman = 'V'
-      ones -= 5
+    roman = ''
+
+    if tens >= 5
+      roman += 'L'
+      tens -= 5
     end
-    if ones >= 4
-      roman = 'IV'
-      ones -= 4
+
+    if tens == 4
+      roman += 'XL'
+      tens -= 4
     end
-    roman += 'I' * ones
-    roman
+
+    roman += 'X' * tens
+    roman + single_digit(ones)
+  end
+
+  def self.single_digit(decimal)
+    return 'IX' if decimal == 9
+    return 'IV' if decimal == 4
+
+    roman = ''
+    if decimal >= 5
+      roman += 'V'
+      decimal -= 5
+    end
+
+    roman + ('I' * decimal)
   end
 end
--- spec/roman_converter_spec.rb
+++ spec/roman_converter_spec.rb
@@ -2,41 +2,28 @@ require "rspec"
 require "pry"
 require_relative "../lib/roman_converter"
 
+input_output_map = {
+  1 => 'I',
+  2 => 'II',
+  3 => 'III',
+  4 => 'IV',
+  5 => 'V',
+  6 => 'VI',
+  9 => 'IX',
+  10 => 'X',
+  14 => 'XIV',
+  15 => 'XV',
+  20 => 'XX',
+  30 => 'XXX',
+  40 => 'XL',
+  50 => 'L',
+  60 => 'LX'
+}
+
 describe RomanConverter do
-  it 'converts 1 to I' do
-    expect(RomanConverter.to_roman(1)).to eq('I')
+  input_output_map.each do |input, output|
+    it "Converts #{input} to #{output}" do
+      expect(RomanConverter.to_roman(input)).to eq(output)
+    end
   end
-
-  it 'converts 2 to II' do
-    expect(RomanConverter.to_roman(2)).to eq('II')
-  end
-
-  it 'converts 3 to III' do
-    expect(RomanConverter.to_roman(3)).to eq('III')
-  end
-
-  it 'converts 5 to V' do
-    expect(RomanConverter.to_roman(5)).to eq('V')
-  end
-
-  it 'converts 6 to VI' do
-    expect(RomanConverter.to_roman(6)).to eq('VI')
-  end
-
-  it 'converts 4 to IV' do
-    expect(RomanConverter.to_roman(4)).to eq('IV')
-  end
-
-  it 'converts 10 to X' do
-    expect(RomanConverter.to_roman(10)).to eq('X')
-  end
-
-  it 'converts 20 to XX' do
-    expect(RomanConverter.to_roman(20)).to eq('XX')
-  end
-
-  # TODO:
-  # 9 -> IX
-  # 40 -> XL
-  # 50 -> L
 end
